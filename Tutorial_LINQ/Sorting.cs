0c188b1b2505720059ce1b9ff9046f14d219e9f6
Gon <krdn.net@gmail.com>
2023-09-15 23:02:51 +0900

    주석 추가

--- Tutorial_LINQ/Sorting.cs
+++ Tutorial_LINQ/Sorting.cs
@@ -5,21 +5,22 @@ internal class Sorting
     {
         string[] words = { "the", "quick", "brown", "fox", "jumps"};
 
+        // 1. 길이로 정렬
         //IOrderedEnumerable<string> query = from word in words
         //    orderby word.Length descending
         //    select word;
 
+        // 1. 길이로 정렬
+        // 2. 길이가 같으면 첫글자로 정렬
         IOrderedEnumerable<string> query = from word in words
             orderby word.Length, word.Substring(0, 1)
             select word;
-
-
         Console.WriteLine();
         Console.WriteLine("Sort 사용**************************");
 
         foreach (var word in query)
         {
-            Console.Write($"{word} ");
+            Console.Write($"{word}\n");
         }
 
         Console.ReadKey();
